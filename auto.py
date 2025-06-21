b1a7f0027ff5c918d9173c8ea26eef1c76b1b673
majoend <majojanuary32@gmail.com>
2025-06-21 18:24:53 -0400

    oop

---
+++ auto.py
@@ -0,0 +1,13 @@
+
+class Auto:
+    def __init__(self, marca, color, year):
+       
+        self.marca = marca
+        self.color = color
+        self.year = year
+
+    def encendido(self):
+        print('El auto esta encendido')
+    
+    def apagado(self):
+        print('El auto esta apagado')
--- create.py
+++ create.py
@@ -1,3 +1,14 @@
 
 #LAMBDA
 #ESTAMOS CREANDO COSAS CON LA FUNCION LAMBDA
+
+from auto import Auto
+
+auto_1 = Auto("Chevrolet", "Azul", 2020)
+
+print(auto_1.marca)
+print(auto_1.color)
+print(auto_1.year)
+
+auto_1.apagado()
+auto_1.encendido()
