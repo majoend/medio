0f79fcfd2aee04ae5a8b5e836b3cbb6ef773c7c2
majoend <majojanuary32@gmail.com>
2025-07-08 20:37:26 -0400

    class

--- poo.py
+++ poo.py
@@ -1,9 +1,36 @@
 
-#el celular
 #atributos ----> son las caracteristicas
 #metodos -----> son las acciones que hacen
 
-class Libros:
-    pass
+class Animal():
+    def dormir(self):
+        print('Este animal esta durmiendo')
+#creo que estoy entendiendo las instancias, es para poder usar la clase creo
+animal = Animal()
+
+#quiero llamar a los metodos, osea lo que esta dentro de esa clase en la funcion 
+animal.dormir()
+
+#creo que asi ya funciona
+#luego quisiera crear mas clases e iria haciendo lo mismo
 
 
+#Ahora creare unas clases que hereden, y hereden a ver como quedan luego
+
+class Seres(): #esta seria la clase principal
+    vivos = True
+
+class Personas(Seres): #esta clase hereda a la primera
+    def caminar(self):
+        print('Las personas caminan')
+
+class Hijas(Personas): #luego hijas pasó a heredar de personas
+    def estudiar(self):
+        print('Las hijas estudian')
+
+vivas = Seres()
+personas = Personas()
+hijas = Hijas()
+
+personas.caminar()
+hijas.estudiar()
