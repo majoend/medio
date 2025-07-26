9007052c01624115883b1b740acb47325273c34e
majoend <majojanuary32@gmail.com>
2025-07-26 14:05:24 -0400

    isaac

---
+++ Isaac.py
@@ -0,0 +1,23 @@
+
+perros = {'pequeños': ['chihuahua', 'pomerania', 'yorkshire'],
+          'medianos': ['bulldog', 'beagle', 'cocker'],
+          'grande': ['pastor alemán', 'labrador', 'dogo']}
+
+def validar_perro(tamaño, raza):
+    if tamaño in perros:
+        if raza in perros[tamaño]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+print(validar_perro('medianos', 'beagle'))
+print(validar_perro('grande', 'chihuahua'))
+
+
+
+
+
+
+    
